a1b256dbed498ab907e4c00dd0f945b67cbe0824
SafnasKaldeen <110978617+SafnasKaldeen@users.noreply.github.com>
2023-09-18 19:33:47 +0530

    DB connect

--- views/assets/js/DB_Connect.js
+++ views/assets/js/DB_Connect.js
@@ -1,15 +1,18 @@
+require("dotenv").config();
 const mysql = require("mysql");
 
 // Create Connection
-const db = mysql.createConnection({
-  host: "aws.connect.psdb.cloud",
-  user: "enpau402qtun4bsfdupw",
-  password: "pscale_pw_x995CGTdUC5Mskv7KSxQwH5rXNw7gMXyZl1V7wX9ECC",
-  database: "ugaa-book-cloud",
-  ssl: {
-    rejectUnauthorized: true,
-  },
-});
+// const db = mysql.createConnection({
+//   host: "aws.connect.psdb.cloud",
+//   user: "8bgb8l0pdd5gt49ia5zt",
+//   password: "pscale_pw_7yFXeMSk3cE55unbld8YCP3ZFmMda4VEJxOs8feJKFD",
+//   database: "ugaa-book-cloud",
+//   ssl: {
+//     rejectUnauthorized: true,
+//   },
+// });
+
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 // Connect to the database
 db.connect((err) => {
