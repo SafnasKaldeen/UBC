0558d60c4ea971b0d0db79e58817149b67440a2f
SafnasKaldeen <110978617+SafnasKaldeen@users.noreply.github.com>
2023-09-18 15:59:55 +0530

    check

--- views/assets/js/DB_Connect.js
+++ views/assets/js/DB_Connect.js
@@ -1,9 +1,23 @@
 const mysql = require("mysql");
 
-//Create Connection
-const db = mysql.createConnection(
-  `mysql://vl4n0ymzbiecs6u5sgot:pscale_pw_PNgsAefk12KJIrzEhRvMyep6aJdVJC2nlIv9lORq1SZ@aws.connect.psdb.cloud/ugaa-book-cloud?ssl={"rejectUnauthorized":true}"`
-);
-console.log("Connected to PlanetScale!");
+// Create Connection
+const db = mysql.createConnection({
+  host: "aws.connect.psdb.cloud",
+  user: "vl4n0ymzbiecs6u5sgot",
+  password: "pscale_pw_PNgsAefk12KJIrzEhRvMyep6aJdVJC2nlIv9lORq1SZ",
+  database: "ugaa-book-cloud",
+  ssl: {
+    rejectUnauthorized: true,
+  },
+});
+
+// Connect to the database
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database:", err);
+  } else {
+    console.log("Connected to PlanetScale!");
+  }
+});
 
 module.exports = db;
