2ceb912004e6921609cc7e51a1890d6374ad4923
SafnasKaldeen <110978617+SafnasKaldeen@users.noreply.github.com>
2023-09-18 21:06:44 +0530

    AWS

--- views/assets/js/DB_Connect.js
+++ views/assets/js/DB_Connect.js
@@ -1,8 +1,13 @@
-require("dotenv").config();
 const mysql = require("mysql");
 
 // Create Connection
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+  host: "ugaa-books-cloud.c9tzw0hrc9md.eu-north-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
+  password: "UgaaBookCloudAdminToken",
+  database: "ugaa_books_cloud",
+});
 
 // Connect to the database
 db.connect((err) => {
